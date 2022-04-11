f2bc0fdbc4d1bed459637effdac44e823120a46d
dvthnight <davide.lanotte@protonmail.com>
2022-04-11 15:30:21 +0200

    creato secondo file blade collegato a route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/movies',function(){
+    return view('movies');
 });
